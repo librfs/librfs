b530ee5f41350570935754c25de673bebee2c0b6
Canmi <9997200@qq.com>
2025-08-09 13:56:44 +0800

    chore: test bump

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 // Copyright (c) 2025 Canmi
 
+use rfs_utils::{log, LogLevel};
+
 fn main() {
-    println!("Hello, world!");
+    log(LogLevel::Info, "This is info message");
+    log(LogLevel::Debug, "This is debug message");
+    log(LogLevel::Warn, "This is warn message");
+    log(LogLevel::Error, "This is error message");
 }
